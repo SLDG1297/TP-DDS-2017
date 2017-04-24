b5c4d14093bd761c073d94d702723310fdb273d7
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-04-24 02:53:07 -0300

    Completo las vistas necesarias para esta entrega del TP. Falta bindearlas.

---
+++ TPA/src/main/java/sistemaDeInversiones/ViewConsultarCuentas.java
@@ -0,0 +1,24 @@
+package sistemaDeInversiones;
+
+import org.uqbar.arena.widgets.Label;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.Window;
+import org.uqbar.arena.windows.WindowOwner;
+
+public class ViewConsultarCuentas extends Window<Empresa> {
+
+	public ViewConsultarCuentas(WindowOwner panelGestionar, Empresa unaEmpresa) {
+		super(panelGestionar, unaEmpresa);
+	}
+
+	@Override
+	public void createContents(Panel panelConsultar) {
+
+		this.setTitle("Consultar cuentas de una empresa");
+		
+		new Label(panelConsultar).setText("Estado de las cuentas:");
+
+		//Muestra una tabla con las cuentas de las empresas.
+		
+	}
+}
