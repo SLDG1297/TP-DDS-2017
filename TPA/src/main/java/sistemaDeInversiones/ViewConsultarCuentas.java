8a74c73e6fe6674edb51c27f721a759a280b4fc6
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-04-24 03:04:18 -0300

    Simplifiqué la clase ViewGestionar.

--- TPA/src/main/java/sistemaDeInversiones/ViewConsultarCuentas.java
+++ TPA/src/main/java/sistemaDeInversiones/ViewConsultarCuentas.java
@@ -16,7 +16,7 @@ public class ViewConsultarCuentas extends Window<Empresa> {
 
 		this.setTitle("Consultar cuentas de una empresa");
 		
-		new Label(panelConsultar).setText("Estado de las cuentas:");
+		new Label(panelConsultar).setText("Estado de las cuentas");
 
 		//Muestra una tabla con las cuentas de las empresas.
 		
--- TPA/src/main/java/sistemaDeInversiones/ViewGestionar.java
+++ TPA/src/main/java/sistemaDeInversiones/ViewGestionar.java
@@ -4,35 +4,28 @@ import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.Selector;
-import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.Window;
 import org.uqbar.arena.windows.WindowOwner;
 
-public class ViewGestionar extends SimpleWindow<Empresa> {
+public class ViewGestionar extends Window<Empresa> {
 
 	public ViewGestionar(WindowOwner panelPrincipal, Empresa unaEmpresa) {
 		super(panelPrincipal, unaEmpresa);
 	}
 
 	@Override
-	public void createContents(Panel mainPanel) {
-		super.createContents(mainPanel);
-	}
-	
-	@Override
-	protected void addActions(Panel panelAcciones) {
-		
-	}
-
-	@Override
-	protected void createFormPanel(Panel panelGestionar) {
+	public void createContents(Panel panelGestionar) {
 		this.setTitle("Gestionar estado de las empresas");
 
-		new Label(panelGestionar).setText("Listado de empresas:\n");
+		new Label(panelGestionar).setText("Listado de empresas");
 
 		new Selector<Empresa>(panelGestionar);
-		
+
 		new Label(panelGestionar).setText("Acciones");
+
+		new Button(panelGestionar).setCaption("Ingresar cuentas");
 		
-		new Button(panelGestionar);
+		new Button(panelGestionar).setCaption("Consultar cuentas")
+			.onClick(() -> new ViewConsultarCuentas(this, new Empresa()).open());
 	}
 }
