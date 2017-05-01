8776fed6f1208ec41d72a53bb53887e856bd3aed
Nachito <igfernandezsoto@gmail.com>
2017-05-01 17:08:55 -0300

    El parser dada una lista de lista de Strings (empresas varias e inclusive repetidas), puede pasar todo a una lista de empresas

--- TPA/src/main/java/sistemaDeInversiones/Parser.java
+++ TPA/src/main/java/sistemaDeInversiones/Parser.java
@@ -2,10 +2,23 @@ package sistemaDeInversiones;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parser {
+	
+	public List<Empresa> aEmpresas(List<List<String>> empresasAParsear) {
+		List<Empresa> misEmpresas = new ArrayList<Empresa>();
+		
+		for(int i = 0; i < empresasAParsear.size(); i++) {
+			misEmpresas.add(this.aEmpresa(empresasAParsear.get(i)));
+		}
+		
+		misEmpresas = this.combinarEmpresas(misEmpresas);
+		
+		return misEmpresas;
+	}
 
-	public Empresa aEmpresa(List<String> empresaAParsear) {
+	private Empresa aEmpresa(List<String> empresaAParsear) {
 		Empresa miEmpresa;
 		
 		miEmpresa = new Empresa(this.nombreDeEmpresa(empresaAParsear), this.periodoDe(empresaAParsear));
@@ -32,8 +45,8 @@ public class Parser {
 		
 		return misCuentas;
 	}
-	
-	// Metodos auxiliares
+		
+	// Metodos auxiliares PRIMERA PARTE
 	
 	private String nombreDeEmpresa(List<String> empresaAParsear) {
 		return empresaAParsear.get(0);
@@ -49,6 +62,81 @@ public class Parser {
 
 	private Integer valorDeCuenta(List<String> empresaAParsear) {
 		return Integer.parseInt(empresaAParsear.get(3));
+	}
+	
+	// Metodos auxiliares SEGUNDA PARTE
+	
+	private List<Empresa> combinarEmpresas(List<Empresa> empresasAParsear) {
+		List<Empresa> misEmpresas = new ArrayList<Empresa>();
+		
+		List<String> nombresDeEmpresa = new ArrayList<String>();
+		
+		nombresDeEmpresa = this.obtenerNombresDeEmpresa(misEmpresas);
+		
+		misEmpresas = this.formarEmpresasDefinitivas(nombresDeEmpresa, misEmpresas);
+		
+		return misEmpresas;
+	}
+	
+	private List<String> obtenerNombresDeEmpresa(List<Empresa> empresasAParsear) {
+		List<String> nombresDeEmpresa = new ArrayList<String>();
+		
+		for(int i = 0; i < empresasAParsear.size(); i++) {
+			String nombreDeLaEmpresa = empresasAParsear.get(i).getNombre();
+			
+			if(!nombresDeEmpresa.contains(nombreDeLaEmpresa)) {
+				nombresDeEmpresa.add(nombreDeLaEmpresa);
+			}
+		}
+		
+		return nombresDeEmpresa;
+	}
+	
+	private List<Empresa> formarEmpresasDefinitivas(List<String> nombresDeEmpresa, List<Empresa> empresasAParsear) {
+		List<Empresa> misEmpresas = new ArrayList<Empresa>();
+		
+		for(int i = 0; i < nombresDeEmpresa.size(); i++) {
+			String nombreDeEmpresa = nombresDeEmpresa.get(i);
+			List<Empresa> empresasConMismoNombre = new ArrayList<Empresa>();
+			
+			empresasConMismoNombre = empresasAParsear.stream().filter(e -> e.getNombre() == nombreDeEmpresa).collect(Collectors.toList());
+			
+			Empresa empresaDefinitiva = this.formarEmpresaDefinitiva(empresasConMismoNombre);
+			
+			misEmpresas.add(empresaDefinitiva);
+		}
+		
+		return misEmpresas;
+	}
+	
+	private Empresa formarEmpresaDefinitiva(List<Empresa> empresasConMismoNombre) {
+		Empresa empresaDefinitiva = new Empresa(empresasConMismoNombre.get(0).getNombre(), this.formarPeriodos(empresasConMismoNombre));
+		
+		return empresaDefinitiva;
+	}
+	
+	private List<Periodo> formarPeriodos(List<Empresa> empresasConMismoNombre) {
+		List<Periodo> misPeriodos = new ArrayList<Periodo>();
+
+		for(int i = 0; i < empresasConMismoNombre.size(); i++) {
+			
+			Periodo periodoActual = empresasConMismoNombre.get(i).getPeriodos().get(0);
+			
+			boolean contieneElAnio = misPeriodos.stream().anyMatch(p -> p.getAnio() == periodoActual.getAnio());
+			
+			if(contieneElAnio) {
+				Periodo periodoDeMisPeriodos = misPeriodos.stream().
+						filter(p -> periodoActual.getAnio() == p.getAnio()).
+						collect(Collectors.toList()).get(0);
+				
+				periodoDeMisPeriodos.cuentas.add(periodoActual.getCuentas().get(0));
+			} else {
+				misPeriodos.add(new Periodo(periodoActual.getAnio(), periodoActual.getCuentas()));
+			}
+			
+		}
+		
+		return misPeriodos;
 	}
 	
 	/*
