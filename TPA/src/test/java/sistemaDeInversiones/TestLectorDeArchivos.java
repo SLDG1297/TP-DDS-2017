88016917eec3cb2b05e90d77d6ebf3c8c8f1fae4
Nachito <igfernandezsoto@gmail.com>
2017-05-05 15:41:05 -0300

    Se agregaron tests en el Parser de cuentitas de Hector

--- TPA/src/test/java/sistemaDeInversiones/TestLectorDeArchivos.java
+++ TPA/src/test/java/sistemaDeInversiones/TestLectorDeArchivos.java
@@ -1,5 +1,48 @@
 package sistemaDeInversiones;
 
-public class TestLectorDeArchivos {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLectorDeArchivos {
+	
+	@Test
+	public void ParserYLectorDeArchivos() {
+		List<List<String>> empresasAParsear = new ArrayList<List<String>>();
+		
+		empresasAParsear = LectorDeArchivos.entregarLista();
+		
+	
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(0).getCuentas().get(0).getValor(), 8000);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(0).getValor(), 6969);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(2).getPeriodos().get(1).getCuentas().get(0).getValor(), 9000);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(3).getPeriodos().get(0).getCuentas().get(1).getValor(), 7171);
+	}
+	
+	@Test
+	public void ParserYLectorDeArchivosNachito() {
+		List<List<String>> empresasAParsear = new ArrayList<List<String>>();
+		
+		empresasAParsear = LectorDeArchivos.entregarLista();
+		
+	
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(0).getCuentas().get(0).getValor(), 8000);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(1).getCuentas().get(0).getValor(), 9000);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(0).getValor(), 6969);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(1).getValor(), 7171);
+	}
 }
--- TPA/src/test/java/sistemaDeInversiones/TestParser.java
+++ TPA/src/test/java/sistemaDeInversiones/TestParser.java
@@ -239,21 +239,188 @@ public class TestParser {
 		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(1).getValor(), 1000);
 	}
 	
-	public void ParserYLectorDeArchivos() {
+	@Test
+	public void cuentitasDeHector_valor() {
+		List<String> empresa1 = new ArrayList<String>();
+		List<String> empresa2 = new ArrayList<String>();
+		List<String> empresa3 = new ArrayList<String>();
+		List<String> empresa4 = new ArrayList<String>();
+		
+		empresa1.add("Rolito");
+		empresa1.add("EDITBA");
+		empresa1.add("2000");
+		empresa1.add("8000");
+
+		empresa3.add("Axel's Consortium Bags");
+		empresa3.add("Free Cash Flow");
+		empresa3.add("2017");
+		empresa3.add("6969");
+		
+		empresa2.add("Rolito");
+		empresa2.add("EDITBA");
+		empresa2.add("2001");
+		empresa2.add("9000");
+		
+		empresa4.add("Axel's Consortium Bags");
+		empresa4.add("EDITBA");
+		empresa4.add("2017");
+		empresa4.add("7171");
+		
 		List<List<String>> empresasAParsear = new ArrayList<List<String>>();
 		
-		empresasAParsear = LectorDeArchivos.entregarLista();
+		empresasAParsear.add(empresa1);
+		empresasAParsear.add(empresa2);
+		empresasAParsear.add(empresa3);
+		empresasAParsear.add(empresa4);
 		
-	
 		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(0).getCuentas().get(0).getValor(), 8000);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(1).getCuentas().get(0).getValor(), 9000);
 		
 		
 		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(0).getValor(), 6969);
 		
 		
-		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(2).getPeriodos().get(1).getCuentas().get(0).getValor(), 9000);
-		
-		
-		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(3).getPeriodos().get(0).getCuentas().get(1).getValor(), 7171);
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(1).getValor(), 7171);
+	
 	}
+	
+	@Test
+	public void cuentitasDeHector_tipoDeCuenta() {
+		List<String> empresa1 = new ArrayList<String>();
+		List<String> empresa2 = new ArrayList<String>();
+		List<String> empresa3 = new ArrayList<String>();
+		List<String> empresa4 = new ArrayList<String>();
+		
+		empresa1.add("Rolito");
+		empresa1.add("EDITBA");
+		empresa1.add("2000");
+		empresa1.add("8000");
+
+		empresa3.add("Axel's Consortium Bags");
+		empresa3.add("Free Cash Flow");
+		empresa3.add("2017");
+		empresa3.add("6969");
+		
+		empresa2.add("Rolito");
+		empresa2.add("EDITBA");
+		empresa2.add("2001");
+		empresa2.add("9000");
+		
+		empresa4.add("Axel's Consortium Bags");
+		empresa4.add("EDITBA");
+		empresa4.add("2017");
+		empresa4.add("7171");
+		
+		List<List<String>> empresasAParsear = new ArrayList<List<String>>();
+		
+		empresasAParsear.add(empresa1);
+		empresasAParsear.add(empresa2);
+		empresasAParsear.add(empresa3);
+		empresasAParsear.add(empresa4);
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(0).getCuentas().get(0).getNombre(), "EDITBA");
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(1).getCuentas().get(0).getNombre(), "EDITBA");
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(0).getNombre(), "Free Cash Flow");
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getCuentas().get(1).getNombre(), "EDITBA");
+	
+	}
+	
+	@Test
+	public void cuentitasDeHector_anio() {
+		List<String> empresa1 = new ArrayList<String>();
+		List<String> empresa2 = new ArrayList<String>();
+		List<String> empresa3 = new ArrayList<String>();
+		List<String> empresa4 = new ArrayList<String>();
+		
+		empresa1.add("Rolito");
+		empresa1.add("EDITBA");
+		empresa1.add("2000");
+		empresa1.add("8000");
+
+		empresa3.add("Axel's Consortium Bags");
+		empresa3.add("Free Cash Flow");
+		empresa3.add("2017");
+		empresa3.add("6969");
+		
+		empresa2.add("Rolito");
+		empresa2.add("EDITBA");
+		empresa2.add("2001");
+		empresa2.add("9000");
+		
+		empresa4.add("Axel's Consortium Bags");
+		empresa4.add("EDITBA");
+		empresa4.add("2017");
+		empresa4.add("7171");
+		
+		List<List<String>> empresasAParsear = new ArrayList<List<String>>();
+		
+		empresasAParsear.add(empresa1);
+		empresasAParsear.add(empresa2);
+		empresasAParsear.add(empresa3);
+		empresasAParsear.add(empresa4);
+		
+		Integer anio = 2000;
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(0).getAnio(), anio);
+		
+		anio = 2001;
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getPeriodos().get(1).getAnio(), anio);
+		
+		anio = 2017;
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getPeriodos().get(0).getAnio(), anio);
+			
+	}
+	
+	@Test
+	public void cuentitasDeHector_nombre() {
+		List<String> empresa1 = new ArrayList<String>();
+		List<String> empresa2 = new ArrayList<String>();
+		List<String> empresa3 = new ArrayList<String>();
+		List<String> empresa4 = new ArrayList<String>();
+		
+		empresa1.add("Rolito");
+		empresa1.add("EDITBA");
+		empresa1.add("2000");
+		empresa1.add("8000");
+
+		empresa3.add("Axel's Consortium Bags");
+		empresa3.add("Free Cash Flow");
+		empresa3.add("2017");
+		empresa3.add("6969");
+		
+		empresa2.add("Rolito");
+		empresa2.add("EDITBA");
+		empresa2.add("2001");
+		empresa2.add("9000");
+		
+		empresa4.add("Axel's Consortium Bags");
+		empresa4.add("EDITBA");
+		empresa4.add("2017");
+		empresa4.add("7171");
+		
+		List<List<String>> empresasAParsear = new ArrayList<List<String>>();
+		
+		empresasAParsear.add(empresa1);
+		empresasAParsear.add(empresa2);
+		empresasAParsear.add(empresa3);
+		empresasAParsear.add(empresa4);
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(0).getNombre(), "Rolito");
+		
+		
+		Assert.assertEquals(Parser.aEmpresas(empresasAParsear).get(1).getNombre(), "Axel's Consortium Bags");
+			
+	}
+	
 }
