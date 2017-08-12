b766fc10e8c3412a042abf5c8a9890d448ed2df3
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-08-12 18:57:36 -0300

    Pequeños tests de indicadores en proceso de gestación

---
+++ TPA/src/test/java/TestIndicadores/TestImprimir.java
@@ -0,0 +1,28 @@
+package TestIndicadores;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import Modelo.Indicadores.IndicadorBuilder;
+import Modelo.Indicadores.Numero;
+import Modelo.Indicadores.Suma;
+import org.junit.Assert;
+
+public class TestImprimir {
+	IndicadorBuilder indicador = new IndicadorBuilder();
+	
+	@Test
+	public void sePuedeImprimirVacio(){
+		indicador.setOperandoAnterior(null);
+		
+		Assert.assertEquals("", indicador.getOperandoAnterior().imprimirFormula());
+	}
+	
+	@Test
+	public void sePuedeImprimirEDITBAmas(){
+		indicador.setOperandoAnterior(new Suma(new Numero(new BigDecimal(2000))));
+		
+		Assert.assertEquals("2000 + ", indicador.getOperandoAnterior().imprimirFormula());
+	}
+}
