9cd698d6d7118e03f24975dc9610b7c395351b37
yankkok <igfernandezsoto@gmail.com>
2017-08-22 16:04:33 -0300

    asdddddddddddgit add .!

--- TPA/src/test/java/TestIndicadores/TestImprimir.java
+++ TPA/src/test/java/TestIndicadores/TestImprimir.java
@@ -16,15 +16,13 @@ public class TestImprimir {
 	public void sePuedeImprimirVacio(){
 		indicador.setOperandoAnterior(null);
 		
-		Assert.assertEquals("", indicador.getOperandoAnterior().imprimirFormula());
+		Assert.assertEquals("", indicador.imprimirFormula());
 	}
 	
 	@Test
 	public void sePuedeImprimirEDITBAmas(){
 		indicador.setOperandoAnterior(new Suma(new Numero(new BigDecimal(2000))));
-		
-		System.out.println(indicador.imprimirFormula());
-		
+
 		Assert.assertEquals("2000 + ", indicador.getOperandoAnterior().imprimirFormula());
 	}
 }
