f0377a4babe7d7ea625204436cf3b306b0ad72c3
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-09-27 01:58:29 -0300

    Corregí un error del RunTimeAdapterFactory y ahora sí funciona la persistencia de indicadores con Converter. Falta hacer lo de indicadores anidados

--- TPA/src/main/java/DB/Serializador/AdaptadorJson.java
+++ TPA/src/main/java/DB/Serializador/AdaptadorJson.java
@@ -1,6 +1,5 @@
 package DB.Serializador;
 
-import Modelo.Empresa.Cuenta;
 import Modelo.Indicadores.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,13 +9,15 @@ public class AdaptadorJson {
     private RuntimeTypeAdapterFactory<Expresiones> adapter(){
         RuntimeTypeAdapterFactory<Expresiones> runtimeTypeAdapterFactory = RuntimeTypeAdapterFactory
                 .of(Expresiones.class, "type")
+                .registerSubtype(Numero.class, "Numero")
+                .registerSubtype(Cuenta_Indicadores.class, "Cuenta")
+                .registerSubtype(Operacion.class, "Operacion")
                 .registerSubtype(Suma.class, "Suma")
                 .registerSubtype(Resta.class, "Resta")
-                .registerSubtype(Numero.class, "Numero")
-                .registerSubtype(Indicador.class, "Indicador")
                 .registerSubtype(Division.class, "Division")
                 .registerSubtype(Multiplicacion.class, "Multiplicacion")
-                .registerSubtype(Cuenta_Indicadores.class, "Cuenta");
+                .registerSubtype(Indicador.class, "Indicador");
+                
         return runtimeTypeAdapterFactory;
     }
 
