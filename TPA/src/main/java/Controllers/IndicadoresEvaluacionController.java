3a41272c700430d731d3eb50587f024bd4d48f41
Ignacio Javier Fernández Soto <ignacio.fernandez@despegar.com>
2017-10-18 09:55:34 -0300

    Primer approach consulta de indicadores

--- TPA/src/main/java/Controllers/IndicadoresEvaluacionController.java
+++ TPA/src/main/java/Controllers/IndicadoresEvaluacionController.java
@@ -1,15 +1,22 @@
 package Controllers;
 
+import DB.Repositorios.RepositorioIndicadores;
 import Modelo.GestorDeUsuarios;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import java.util.Map;
+
 public class IndicadoresEvaluacionController {
 
 	   public ModelAndView show(Request request, Response response){
 
-			return new ModelAndView(GestorDeUsuarios.getInstance().obtenerMapa(request),"indicadoresEvaluacion.hbs");
+		   Map<Object, Object> mapa = GestorDeUsuarios.getInstance().obtenerMapa(request);
+
+		   mapa.put("indicador", RepositorioIndicadores.getInstancia().buscarListaDeObjetos());
+
+		   return new ModelAndView(mapa,"indicadoresEvaluacion.hbs");
 
 	   }
 	
