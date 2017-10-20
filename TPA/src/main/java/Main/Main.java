fa6f7f2f30904d497bc0ee98d6b9119f624229da
Erik-1012 <bernaderi@hotmail.com>
2017-10-20 06:31:37 -0300

    No se qué pasa

--- TPA/src/main/java/Main/Main.java
+++ TPA/src/main/java/Main/Main.java
@@ -1,14 +1,11 @@
 package Main;
 
-import DB.Proveedores.ProveedorBD;
-import DB.Repositorios.RepositorioEmpresas;
-import Modelo.Empresa.Empresa;
 import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Bootstrap.iniciarModelo();
-		RepositorioEmpresas.getInstancia().setProveedor(new ProveedorBD<Empresa>());
+		
 		WebMain.iniciarInterfazWeb();
 	}
 
