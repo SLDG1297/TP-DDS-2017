1e3b8bb75ee105d38bb85e3567bcad2fac937cb1
Jesus Ledesma <jh.ledesmaa@gmail.com>
2017-11-18 03:36:02 -0300

    Herramientas para trabajar con el repo de precalculados

--- TPA/src/main/java/DB/Proveedores/ProveedorMongoDB.java
+++ TPA/src/main/java/DB/Proveedores/ProveedorMongoDB.java
@@ -2,9 +2,29 @@ package DB.Proveedores;
 
 import DB.MongoDBManager;
 import DB.TiposDeRepositorios.TipoDeRepositorio;
+import com.google.gson.Gson;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
 
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class ProveedorMongoDB<T extends TipoDeRepositorio> extends MongoDBManager implements Proveedor<T> {
@@ -13,31 +33,44 @@ public class ProveedorMongoDB<T extends TipoDeRepositorio> extends MongoDBManage
 
     private MongoCollection<Document> collection = null;
 
+    private void comprobarCollection(String unTipo){
+        if(collection == null)
+        { collection = crearColeccion(unTipo);}
+    }
+
+    private Object jsonToObjeto(String json,String unTipo) throws ClassNotFoundException {
+        Gson gson = new Gson();
+        Class c = Class.forName(unTipo);
+        return gson.fromJson(json, c.getClass());
+    }
     @Override
     public T darObjeto(String unNombre, String unTipo) {
-        if(collection == null)
-        {
-            collection = crearColeccion(unTipo);
-        }
-
+        this.comprobarCollection(unTipo);
         return null;
     }
 
     @Override
     public List<T> darLista(String unTipo) {
-        if(collection == null)
-        {
-            collection = crearColeccion(unTipo);
+        this.comprobarCollection(unTipo);
+        List<Object> lista = new ArrayList<Object>();
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+               String o = cursor.next().toJson();
+               Object objeto = this.jsonToObjeto(o,unTipo);
+               lista.add(objeto);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            cursor.close();
         }
-        return null;
+        return (List<T>) lista;
     }
 
     @Override
     public List<String> darListaNombres(String unTipo) {
-        if(collection == null)
-        {
-            collection = crearColeccion(unTipo);
-        }
+        this.comprobarCollection(unTipo);
         return null;
     }
 
